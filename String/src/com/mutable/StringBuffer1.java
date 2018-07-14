5ca9a30ce9e2a289ab38f9beb5167c5693cd006f
chaman bharti <chaman.bharti84@gmail.com>
2018-07-14 13:24:28 +0530

    dfgfdccbv

--- String/src/com/mutable/StringBuffer1.java
+++ String/src/com/mutable/StringBuffer1.java
@@ -4,6 +4,8 @@ public class StringBuffer1 {
 	
 	public static void main(String args[]){  
 		StringBuffer sb=new StringBuffer("Hello StringBuffer");  
+		sb.reverse();
+		System.out.println(sb);
 		sb.delete(1,3);  
 		System.out.println(sb);//prints Hlo  
 		}  
