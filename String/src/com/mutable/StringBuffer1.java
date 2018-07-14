61226f08290d291536bedd9508469ea155ef7c14
chaman bharti <chaman.bharti84@gmail.com>
2018-07-14 13:22:06 +0530

    hey

--- String/src/com/mutable/StringBuffer1.java
+++ String/src/com/mutable/StringBuffer1.java
@@ -3,7 +3,7 @@ package com.mutable;
 public class StringBuffer1 {
 	
 	public static void main(String args[]){  
-		StringBuffer sb=new StringBuffer("Hello Buffer");  
+		StringBuffer sb=new StringBuffer("Hello StringBuffer");  
 		sb.delete(1,3);  
 		System.out.println(sb);//prints Hlo  
 		}  
